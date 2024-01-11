f9cebe1137cabef53322965b97ecf8dfae7d1b00
rojoyin <rojoyin@gmail.com>
2024-01-11 17:38:11 -0500

    Implement playwright service for open pages

--- app/routers/scraping.py
+++ app/routers/scraping.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from typing import Dict
+
+from fastapi import APIRouter, HTTPException
 
 from app.schemas.url import UrlModel
+from app.services.playwright import scrape_with_playwright_async
 
 router = APIRouter()
 
 
 @router.post("/scrape/")
-async def scrape_url(target_url: UrlModel):
-    return {"url": target_url, "content": "Scraped content"}
+async def scrape_url(item: UrlModel) -> Dict[str, str]:
+    try:
+        url = str(item.url)
+        content = await scrape_with_playwright_async(url)
+        return {"data": content}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ app/services/playwright.py
@@ -0,0 +1,11 @@
+from playwright.async_api import async_playwright
+
+
+async def scrape_with_playwright_async(url: str):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        await page.goto(url)
+        content = await page.content()
+        await browser.close()
+        return content
