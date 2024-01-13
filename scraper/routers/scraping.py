774053e8745e47a5d86d755fd5c1338033418164
rojoyin <rojoyin@gmail.com>
2024-01-13 10:03:28 -0500

    Refactorr service to scrape recaptcha protected site

--- scraper/routers/scraping.py
+++ scraper/routers/scraping.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict
 
 from fastapi import APIRouter, HTTPException
@@ -6,12 +7,14 @@ from scraper.schemas.url import UrlModel
 from scraper.services.playwright import scrape_with_playwright_async
 
 router = APIRouter()
+LOG = logging.getLogger(__name__)
 
 
 @router.post("/scrape/")
 async def scrape_url(item: UrlModel) -> Dict[str, str]:
     try:
         url = str(item.url)
+        LOG.info(f"Receive URL to scrape: {url}")
         content = await scrape_with_playwright_async(url)
         return {"data": content}
     except Exception as e:
--- scraper/services/playwright.py
+++ scraper/services/playwright.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import random
 import urllib.request
 
@@ -6,42 +7,90 @@ import pydub
 from playwright.async_api import async_playwright
 from speech_recognition import AudioFile, Recognizer
 
-
 LOG = logging.getLogger(__name__)
-LOG.setLevel(logging.INFO)
+TEMP_MP3_FILE = "audio.mp3"
+TEMP_WAV_FILE = "audio.wav"
+
+
+async def clean_up():
+    LOG.info("Remove intermediate files")
+    if os.path.exists(TEMP_MP3_FILE):
+        os.remove(TEMP_MP3_FILE)
+    if os.path.exists(TEMP_WAV_FILE):
+        os.remove(TEMP_WAV_FILE)
+
+
+class ReCaptchaSolver:
+
+    def __init__(self, playwright, url):
+        self.recognizer = Recognizer()
+        self.playwright = playwright
+        self.url = url
+        self.browser = None
+        self.page = None
+        LOG.info("Created solver class")
+
+    async def setup_page(self):
+        LOG.info("Navigating to URL")
+        self.browser = await self.playwright.chromium.launch(headless=False)
+        self.page = await self.browser.new_page()
+        await self.page.goto(self.url)
+
+    def convert_speech_to_text(self, mp3_file):
+        try:
+            LOG.info("Downloading audio")
+            pydub.AudioSegment.from_mp3(mp3_file).export(TEMP_WAV_FILE, format="wav")
+            recaptcha_audio = AudioFile(TEMP_WAV_FILE)
+            LOG.info("Converting audio to text")
+            with recaptcha_audio as source:
+                audio = self.recognizer.record(source)
+            text = self.recognizer.recognize_google(audio)
+            LOG.info(f"Recognized text: {text}")
+            return text
+        except Exception as e:
+            LOG.error("Error while processing audio", e)
+
+    async def random_delay(self):
+        waiting_time = 1000 * random.randint(1, 3)
+        LOG.info(f"Waiting for {waiting_time} in milliseconds")
+        await self.page.wait_for_timeout(random.randint(1, 3) * 1000)
+
+    async def solve(self):
+        LOG.info("Attempting to solve the captcha")
+
+        # find the recaptcha iframe
+        recaptcha_frame_name = await self.page.locator("//iframe[@title='reCAPTCHA']").get_attribute("name")
+        captcha = self.page.frame(name=recaptcha_frame_name)
+        await captcha.click("//div[@class='recaptcha-checkbox-border']")
+        await self.random_delay()
+
+        # find the challenge iframe
+        challenge_frame_name = await self.page.locator(
+            "//iframe[contains(@src,'https://www.google.com/recaptcha/api2/bframe?')]").get_attribute(
+            "name")
+        challenge_frame = self.page.frame(name=challenge_frame_name)
+        await challenge_frame.click("id=recaptcha-audio-button")
+        await challenge_frame.click("//button[@aria-labelledby='audio-instructions rc-response-label']")
+        href = await challenge_frame.locator("//a[@class='rc-audiochallenge-tdownload-link']").get_attribute("href")
+        urllib.request.urlretrieve(href, TEMP_MP3_FILE)
+        text = self.convert_speech_to_text(TEMP_MP3_FILE)
+        await challenge_frame.fill("id=audio-response", text)
+        await challenge_frame.click("id=recaptcha-verify-button")
+        await self.random_delay()
+        submit_button = self.page.locator('[type="submit"]')
+        await submit_button.click()
+        await self.random_delay()
+        content = await self.page.content()
+        return content
 
 
 async def scrape_with_playwright_async(url: str):
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
-        page = await browser.new_page()
-        await page.goto(url)
-        name = await page.locator("//iframe[@title='reCAPTCHA']").get_attribute("name")
-        captcha = page.frame(name=name)
-        await captcha.click("//div[@class='recaptcha-checkbox-border']")
-        await page.wait_for_timeout(random.randint(1, 3) * 1000)
-        name_ = await page.locator(
-            "//iframe[contains(@src,'https://www.google.com/recaptcha/api2/bframe?')]").get_attribute(
-            "name")
-        main_frame = page.frame(name=name_)
-        await main_frame.click("id=recaptcha-audio-button")
-        await main_frame.click("//button[@aria-labelledby='audio-instructions rc-response-label']")
-        href = await main_frame.locator("//a[@class='rc-audiochallenge-tdownload-link']").get_attribute("href")
-        urllib.request.urlretrieve(href, "audio.mp3")
-        pydub.AudioSegment.from_mp3("audio.mp3").export("audio.wav", format="wav")
-        recognizer = Recognizer()
-        recaptcha_audio = AudioFile("audio.wav")
-        with recaptcha_audio as source:
-            audio = recognizer.record(source)
-
-        text = recognizer.recognize_google(audio)
-        LOG.info(f"Recognized text: {text}")
-        await main_frame.fill("id=audio-response", text)
-        await main_frame.click("id=recaptcha-verify-button")
-        await page.wait_for_timeout(1000 * random.randint(1, 3))
-        submit_button = page.locator('[type="submit"]')
-        await submit_button.click()
-        await page.wait_for_timeout(1000 * random.randint(1, 3))
-        content = await page.content()
-        await browser.close()
-        return content
+        try:
+            solver = ReCaptchaSolver(p, url)
+            await solver.setup_page()
+            content = await solver.solve()
+            await clean_up()
+            return content
+        finally:
+            await clean_up()
